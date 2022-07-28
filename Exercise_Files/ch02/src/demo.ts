5588d5f6f2a24a93126ae016616145ee5e5ccdaf
liu-minghua <liu_minghua@hotmail.com>
2022-07-28 08:55:13 -0500

    exercise

---
+++ Exercise_Files/ch02/src/demo.ts
@@ -0,0 +1,22 @@
+let x: number
+let y: string
+let strs: string[]
+let b: boolean
+let dt: Date
+let a: any
+
+a = "Hello"
+
+a = 4
+
+a = true
+
+strs = "Hello!" as any
+
+x = 1234
+
+y = "Hello"
+
+b = true
+
+dt = new Date
