34f4cbc2f2547aa3c35c7683aa62f8426724b246
liu-minghua <liu_minghua@hotmail.com>
2022-08-01 12:49:18 -0500

    exercise on limit generic type by extends

--- Exercise_Files/ch02/src/demo7.ts
+++ Exercise_Files/ch02/src/demo7.ts
@@ -2,16 +2,21 @@ interface Contact {
     id: number,
     name: string
 }
+interface UserContact {
+    id: number,
+    name: string,
+    username: string
+}
 interface DateRange {
     startDate: Date,
     endDate: Date
 }
-function clone<T1, T2>(src: T1): T2{
+function clone<T1, T2 extends T1>(src: T1): T2{
     return Object.apply({}, src);
 }
 
 const a: Contact = { id: 1234, name: "Mark Liu" }
-const b = clone<Contact, Contact>(a)
+const b = clone<Contact, UserContact>(a)
 
 const dateRange: DateRange = {startDate: new Date, endDate: new Date}
 const dateRangeCopy = clone<DateRange, DateRange>(dateRange)
